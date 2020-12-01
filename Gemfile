44a62215367596d20da4734ea9b43d96bbfc6c65
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-12-01 19:11:37 +0900

    編集

--- Gemfile
+++ Gemfile
@@ -56,3 +56,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'line-bot-api'
 
 
+gem 'dotenv-rails'
+
+gem 'jwt'
--- app/controllers/line_events_controller.rb
+++ app/controllers/line_events_controller.rb
@@ -1,10 +1,10 @@
 class LineEventsController < ApplicationController
   require 'line/bot'
 
-  protect_from_forgery except: [:callback]
+  protect_from_forgery :except => [:callback]
 
   def client
-    @client ||=Line::Bot::Client.new { |config| 
+    @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
@@ -15,24 +15,25 @@ class LineEventsController < ApplicationController
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      head :bad_request
+      error 400 do 'Bad Request' end
     end
 
     events = client.parse_events_from(body)
 
-    events.each{ |event|
+    events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
-        when Line::Bot::Event::Message::Text
+        when Line::Bot::Event::MessageType::Text
           message = {
-            type: 'text'
+            type: 'text',
             text: event.message['text']
           }
         end
       end
-    }
-
-    head :ok
+      client.reply_message(event['replyToken'],message)
+    end
+    "OK"
   end
+
 end
